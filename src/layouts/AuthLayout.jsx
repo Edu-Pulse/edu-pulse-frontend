ee331b25b9ac47a36cb8ddd336655ec0f824cc40
Reinandafaris <reinandafari7@gmail.com>
2023-11-22 18:45:30 +0700

    Feat: create LoginPage

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,18 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
+import logo from '../assets/svg/Logo-Belajar.svg';
 
 const AuthLayout = () => {
-	return (
-		<main className="flex h-screen">
-			<section className="w-[55%]">
-				<Outlet />
-			</section>
-			<aside className="w-[45%] h-full bg-darkblue-05 grid place-content-center">
-				<h1 className="text-3xl font-bold text-white">
-					Pragos Academy
-				</h1>
-			</aside>
-		</main>
-	);
+  return (
+    <main className="flex h-screen">
+      <section className="w-[55%] h-full grid place-content-center">
+        <Outlet />
+      </section>
+      <aside className="w-[45%] h-full bg-darkblue-05 grid place-content-center">
+        <img
+          src={logo}
+          alt="logo-belajar"
+        />
+      </aside>
+    </main>
+  );
 };
 
 export default AuthLayout;
