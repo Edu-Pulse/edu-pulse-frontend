9a916c543cf9823a7472c08d47c20c4024056d92
BayuAditiyaa <shinatsuka7@gmail.com>
2023-12-03 21:34:17 +0700

    Feat: integrasi API Login

--- src/pages/OTP.jsx
+++ src/pages/OTP.jsx
@@ -1,40 +1,34 @@
 import Button from "../components/UI/Button";
 import { useNavigate } from "react-router-dom";
-import {
-	ArrowLeftIcon,
-} from "@heroicons/react/24/outline";
+import { ArrowLeftIcon } from "@heroicons/react/24/outline";
 
 const Otp = () => {
   const navigate = useNavigate();
   return (
     <div className="sm:w-[452px] w-[352px] h-[348px]">
-      <button onClick={() => navigate("/auth/register")} className="h-10"><ArrowLeftIcon className="w-6 h-6"/></button>
-      <h1 className="font-Montserrat text-[24px] font-bold leading-[36px] text-darkblue-05 mb-[24px] mt-4">Masukkan OTP</h1>
-      <p className="font-poppins text-center text-[14px] mb-1 font-bold leading-[18px] text-black">Ketik 4 digit kode yang dikirimkan ke J*****@gmail.com</p>
+      <button onClick={() => navigate("/auth/register")} className="h-10">
+        <ArrowLeftIcon className="w-6 h-6" />
+      </button>
+      <h1 className="font-Montserrat text-[24px] font-bold leading-[36px] text-darkblue-05 mb-[24px] mt-4">
+        Masukkan OTP
+      </h1>
+      <p className="font-poppins text-center text-[14px] mb-1 font-bold leading-[18px] text-black">
+        Ketik 4 digit kode yang dikirimkan ke J*****@gmail.com
+      </p>
       <div className="flex justify-center gap-5 mt-5">
-      <input
-					className=
-						"py-1 ps-3.5 w-10 h-10 rounded-xl border-2 border-darkblue-05 focus:outline-darkblue-05 transition-all duration-300 hover:border-gray-400"
-				/>
-      <input
-					className=
-						"py-1 ps-3.5 w-10 h-10 rounded-xl border-2 border-darkblue-05 focus:outline-darkblue-05 transition-all duration-300 hover:border-gray-400"
-				/>
-      <input
-					className=
-						"py-1 ps-3.5 w-10 h-10 rounded-xl border-2 border-darkblue-05 focus:outline-darkblue-05 transition-all duration-300 hover:border-gray-400"
-				/>
-      <input
-					className=
-						"py-1 ps-3.5 w-10 h-10 rounded-xl border-2 border-darkblue-05 focus:outline-darkblue-05 transition-all duration-300 hover:border-gray-400"
-				/>
+        <input className="py-1 ps-3.5 w-10 h-10 rounded-xl border-2 border-darkblue-05 focus:outline-darkblue-05 transition-all duration-300 hover:border-gray-400" />
+        <input className="py-1 ps-3.5 w-10 h-10 rounded-xl border-2 border-darkblue-05 focus:outline-darkblue-05 transition-all duration-300 hover:border-gray-400" />
+        <input className="py-1 ps-3.5 w-10 h-10 rounded-xl border-2 border-darkblue-05 focus:outline-darkblue-05 transition-all duration-300 hover:border-gray-400" />
+        <input className="py-1 ps-3.5 w-10 h-10 rounded-xl border-2 border-darkblue-05 focus:outline-darkblue-05 transition-all duration-300 hover:border-gray-400" />
       </div>
-        <p className="font-poppins text-center text-[14px] mt-[24px] font-normal leading-[18px] text-black">Masukkan Kode OTP dengan Benar!</p>
+      <p className="font-poppins text-center text-[14px] mt-[24px] font-normal leading-[18px] text-black">
+        Masukkan Kode OTP dengan Benar!
+      </p>
       <div className="flex justify-center mt-[48px]">
         <Button className="w-[452px]">Simpan</Button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Otp
+export default Otp;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,17 @@
-import { Link } from "react-router-dom"
-import Button from "../components/UI/Button"
-import Input from "../components/UI/Input"
+import { Link } from "react-router-dom";
+import Button from "../components/UI/Button";
+import Input from "../components/UI/Input";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
+import axios from "axios";
 
 function Register() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [noTlp, setNoTlp] = useState('');
-  const [password, setPassword] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setphone] = useState("");
+  const [city, setcity] = useState("");
+  const [country, setcountry] = useState("");
+  const [password, setPassword] = useState("");
   const [isErrorName, setIsErrorName] = useState(false);
   const [isErrorEmail, setIsErrorEmail] = useState(false);
   const [isErrorTlp, setIsErrorTlp] = useState(false);
@@ -26,7 +29,7 @@ function Register() {
       setCheckName(false);
       setIsErrorName(true);
     }
-    return
+    return;
   };
 
   const handleEmail = () => {
@@ -38,15 +41,15 @@ function Register() {
     }
   };
 
-  const handleNoTlp = () => {
-    if (noTlp.length >= 9) {
+  const handlephone = () => {
+    if (phone.length >= 9) {
       setCheckTlp(true);
       setIsErrorTlp(false);
     } else {
       setCheckTlp(false);
       setIsErrorTlp(true);
     }
-  }
+  };
 
   const handlePassword = () => {
     if (password.length >= 8) {
@@ -54,13 +57,45 @@ function Register() {
     } else {
       setIsErrorPassword(true);
     }
-  }
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      let data = JSON.stringify({
+        name,
+        email,
+        phone,
+        password,
+        city,
+        country,
+      });
+
+      let config = {
+        method: "post",
+        url: `https://pragos-academy-api-production.up.railway.app/register`,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        data: data,
+      };
+
+      await axios.request(config);
+
+      navigate(`/auth/otp/${email}`);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
     <div className="sm:w-[452px] w-[352px] h-[460px]">
-      <h1 className="font-Montserrat text-[24px] font-bold leading-[36px] text-darkblue-05 mb-[24px]">Daftar</h1>
+      <h1 className="font-Montserrat text-[24px] font-bold leading-[36px] text-darkblue-05 mb-[24px]">
+        Daftar
+      </h1>
       <div className="">
-        <Input 
+        <Input
           placeholder="Nama Lengkap"
           type="text"
           label="Nama"
@@ -74,14 +109,23 @@ function Register() {
         />
         {checkName && (
           <span className="sm:ml-[26rem] ml-[19.5rem] h-6 absolute mt-[-1.9rem]">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-            <path d="M10 0C4.5 0 0 4.5 0 10C0 15.5 4.5 20 10 20C15.5 20 20 15.5 20 10C20 4.5 15.5 0 10 0ZM8 15L3 10L4.41 8.59L8 12.17L15.59 4.58L17 6L8 15Z" fill="#188E55"/>
-          </svg>
-        </span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+            >
+              <path
+                d="M10 0C4.5 0 0 4.5 0 10C0 15.5 4.5 20 10 20C15.5 20 20 15.5 20 10C20 4.5 15.5 0 10 0ZM8 15L3 10L4.41 8.59L8 12.17L15.59 4.58L17 6L8 15Z"
+                fill="#188E55"
+              />
+            </svg>
+          </span>
         )}
       </div>
       <div className="flex justify-between mt-[14px]">
-      <Input 
+        <Input
           placeholder="Contoh: johndee@gmail.com"
           type="email"
           label="Email"
@@ -96,49 +140,97 @@ function Register() {
         />
         {checkEmail && (
           <span className="sm:ml-[26rem] ml-[19.5rem] h-6 absolute mt-[2.2rem]">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-            <path d="M10 0C4.5 0 0 4.5 0 10C0 15.5 4.5 20 10 20C15.5 20 20 15.5 20 10C20 4.5 15.5 0 10 0ZM8 15L3 10L4.41 8.59L8 12.17L15.59 4.58L17 6L8 15Z" fill="#188E55"/>
-          </svg>
-        </span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+            >
+              <path
+                d="M10 0C4.5 0 0 4.5 0 10C0 15.5 4.5 20 10 20C15.5 20 20 15.5 20 10C20 4.5 15.5 0 10 0ZM8 15L3 10L4.41 8.59L8 12.17L15.59 4.58L17 6L8 15Z"
+                fill="#188E55"
+              />
+            </svg>
+          </span>
         )}
       </div>
-      <div className="flex justify-between mt-[14px]">
-      </div>
-        <Input 
-          placeholder="08xxxxxxxxx"
+      <div className="flex justify-between mt-[14px]"></div>
+      <Input
+        placeholder="08xxxxxxxxx"
+        type="text"
+        label="Nomor Telepon"
+        name="Nomor Telepon"
+        value={phone}
+        onChange={(e) => {
+          handlephone();
+          setphone(e.target.value);
+        }}
+        isError={isErrorTlp}
+      />
+      {checkTlp && (
+        <span className="sm:ml-[26rem] ml-[19.5rem] h-6 absolute mt-[-1.9rem]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+          >
+            <path
+              d="M10 0C4.5 0 0 4.5 0 10C0 15.5 4.5 20 10 20C15.5 20 20 15.5 20 10C20 4.5 15.5 0 10 0ZM8 15L3 10L4.41 8.59L8 12.17L15.59 4.58L17 6L8 15Z"
+              fill="#188E55"
+            />
+          </svg>
+        </span>
+      )}
+      <div className="flex justify-between mt-[14px]"></div>
+      <Input
+        placeholder="Buat Password"
+        type="password"
+        label="Buat Password"
+        name="Buat Password"
+        value={password}
+        onChange={(e) => {
+          handlePassword();
+          setPassword(e.target.value);
+        }}
+        isError={isErrorPassword}
+      />
+      <div className="">
+        <Input
+          placeholder="Asal city"
           type="text"
-          label="Nomor Telepon"
-          name="Nomor Telepon"
-          value={noTlp}
+          label="Asal city"
+          name="Asal city"
+          value={city}
           onChange={(e) => {
-            handleNoTlp();
-            setNoTlp(e.target.value);
+            handleName();
+            setcity(e.target.value);
           }}
-          isError={isErrorTlp}
+          isError={isErrorName}
         />
-        {checkTlp && (
-          <span className="sm:ml-[26rem] ml-[19.5rem] h-6 absolute mt-[-1.9rem]">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-            <path d="M10 0C4.5 0 0 4.5 0 10C0 15.5 4.5 20 10 20C15.5 20 20 15.5 20 10C20 4.5 15.5 0 10 0ZM8 15L3 10L4.41 8.59L8 12.17L15.59 4.58L17 6L8 15Z" fill="#188E55"/>
-          </svg>
-        </span>
-        )}
-      <div className="flex justify-between mt-[14px]">
       </div>
-        <Input 
-          placeholder="Buat Password" 
-          type="password"
-          label="Buat Password"
-          name="Buat Password"
-          value={password}
+      <div className="">
+        <Input
+          placeholder="Asal country"
+          type="text"
+          label="Asal country"
+          name="Asal country"
+          value={country}
           onChange={(e) => {
-            handlePassword();
-            setPassword(e.target.value);
+            handleName();
+            setcountry(e.target.value);
           }}
-          isError={isErrorPassword}
+          isError={isErrorName}
         />
+      </div>
       <div className="flex justify-center mt-[24px]">
-        <Button onClick={() => navigate('/auth/otp')} className="w-[452px]">Daftar</Button>
+        <Link to={`/auth/otp/${email}`}>
+          <Button className="w-[452px]" onClick={onSubmit}>
+            Daftar
+          </Button>
+        </Link>
       </div>
       <div className="flex justify-center mt-[40px] gap-[8px]">
         <h3>Sudah punya akun?</h3>
@@ -147,7 +239,7 @@ function Register() {
         </Link>
       </div>
     </div>
-  )
+  );
 }
 
-export default Register
+export default Register;
