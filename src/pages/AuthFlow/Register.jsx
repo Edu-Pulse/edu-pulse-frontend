ec2b18a25eefe6de579668bb333a0ba74d7828b4
Reinandafaris <reinandafari7@gmail.com>
2023-12-05 08:18:17 +0700

    Feat: Memperbarui Tampilan Register

--- src/pages/AuthFlow/Register.jsx
+++ src/pages/AuthFlow/Register.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useContext, useState } from "react";
 import { AuthContext } from "@/context/AuthContext";
 import { Axios } from "axios";
+import CheckList from "../../components/UI/CheckList";
 
 function Register() {
   const {
@@ -11,13 +12,15 @@ function Register() {
 		handleEmailValidation,
 		handlePhoneValidation,
 		handlePasswordValidation,
+    handleCityValidation,
+    handleCountryValidation,
 	} = useContext(AuthContext);
 	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
-	const [phone, setphone] = useState("");
+	const [phone, setPhone] = useState("");
 	const [password, setPassword] = useState("");
-	const [city, setcity] = useState("");
-	const [country, setcountry] = useState("");
+	const [city, setCity] = useState("");
+	const [country, setCountry] = useState("");
 	const [error, setError] = useState({
 		isEmailError: false,
 		isNameError: false,
@@ -57,149 +60,219 @@ function Register() {
 	};
 
 	return (
-		<div className="max-w-[452px] w-full">
-			<h1 className="font-Montserrat text-2xl font-bold leading-[36px] text-darkblue-05 mb-6">
-				Daftar
-			</h1>
-			<div className="">
-				<Input
-					placeholder="Nama Lengkap"
-					type="text"
-					label="Nama"
-					name="Nama Lengkap"
-					value={name}
-					onChange={(e) => {
-						setName(e.target.value);
-					}}
-          onBlur={() => handleNameValidation(name, setError)}
-					isError={error && error.isNameError}
-				/>
-				{!error && !error.isNameError && (
-					<span className="sm:ml-[26rem] ml-[19.5rem] h-6 absolute mt-[-1.9rem]">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="20"
-							height="20"
-							viewBox="0 0 20 20"
-							fill="none"
-						>
-							<path
-								d="M10 0C4.5 0 0 4.5 0 10C0 15.5 4.5 20 10 20C15.5 20 20 15.5 20 10C20 4.5 15.5 0 10 0ZM8 15L3 10L4.41 8.59L8 12.17L15.59 4.58L17 6L8 15Z"
-								fill="#188E55"
-							/>
-						</svg>
-					</span>
-				)}
-			</div>
-			<div className="flex justify-between mt-[14px]">
-				<Input
-					placeholder="Contoh: johndee@gmail.com"
-					type="email"
-					label="Email"
-					name="Email"
-					className="w-full"
-					value={email}
-					onChange={(e) => {
-						setEmail(e.target.value);
-					}}
-          onBlur={() => handleEmailValidation(email, setError)}
-					isError={error && error.isEmailError}
-				/>
-				{error && error.isEmailError && (
-					<span className="sm:ml-[26rem] ml-[19.5rem] h-6 absolute mt-[2.2rem]">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="20"
-							height="20"
-							viewBox="0 0 20 20"
-							fill="none"
-						>
-							<path
-								d="M10 0C4.5 0 0 4.5 0 10C0 15.5 4.5 20 10 20C15.5 20 20 15.5 20 10C20 4.5 15.5 0 10 0ZM8 15L3 10L4.41 8.59L8 12.17L15.59 4.58L17 6L8 15Z"
-								fill="#188E55"
-							/>
-						</svg>
-					</span>
-				)}
-			</div>
-			<div className="flex justify-between mt-[14px]"></div>
-			<Input
-				placeholder="08xxxxxxxxx"
-				type="text"
-				label="Nomor Telepon"
-				name="Nomor Telepon"
-				value={phone}
-				onChange={(e) => {
-					setphone(e.target.value);
-				}}
-        onBlur={() => handlePhoneValidation(phone, setError)}
-				isError={error && error.isPhoneError}
-			/>
-			{error && error.isPhoneError && (
-				<span className="sm:ml-[26rem] ml-[19.5rem] h-6 absolute mt-[-1.9rem]">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						width="20"
-						height="20"
-						viewBox="0 0 20 20"
-						fill="none"
+		<div className="h-screen grid place-content-center items-center justify-center">
+      <div className="h-[441px] sm:w-[452px] w-[390px] sm:px-0 px-4">
+
+      {/* Tag Daftar */}
+      <h1 className="font-Montserrat text-[24px] font-bold leading-[36px] text-darkblue-05">
+        Daftar
+      </h1>
+
+      {/* Pembuka Gabungan Nama dan Nomor Telepon */}
+        <div className="mt-1 flex flex-row">
+
+        {/* Input Nama */}
+          <div className="w-[50%] me-1 flex justify-between">
+          <div className="relative">
+            <Input
+              placeholder="Budi"
+              type="text"
+              value={name}
+              label="Nama"
+              name="Nama"
+              onChange={(e) => setName(e.target.value)}
+              onBlur={() => handleNameValidation(name, setError)}
+              isError={error.isNameError}
+            />
+
+          {/* Ketika Nama Error */}
+          {error && error.isNameError && (
+					<label
+						htmlFor="Nama"
+						className="text-xs text-alert-warning"
 					>
-						<path
-							d="M10 0C4.5 0 0 4.5 0 10C0 15.5 4.5 20 10 20C15.5 20 20 15.5 20 10C20 4.5 15.5 0 10 0ZM8 15L3 10L4.41 8.59L8 12.17L15.59 4.58L17 6L8 15Z"
-							fill="#188E55"
-						/>
-					</svg>
-				</span>
-			)}
-			<div className="flex justify-between mt-[14px]"></div>
-			<Input
-				placeholder="Buat Password"
-				type="password"
-				label="Buat Password"
-				name="Buat Password"
-				value={password}
-				onChange={(e) => {
-					setPassword(e.target.value);
-				}}
-				onBlur={() => handlePasswordValidation(password, setError)}
-				isError={error && error.isPasswordError}
-			/>
-			<div className="">
-				<Input
-					placeholder="Jakarta"
-					type="text"
-					label="Asal Kota"
-					name="Asal Kota"
-					value={city}
-					onChange={(e) => {
-						setcity(e.target.value);
-					}}
-				/>
-			</div>
-			<div className="">
-				<Input
-					placeholder="Asal Indonesia"
-					type="text"
-					label="Asal negara"
-					name="Asal negara"
-					value={country}
-					onChange={(e) => {
-						setcountry(e.target.value);
-					}}
-				/>
-			</div>
-			<div className="flex justify-center mt-[24px]">
-				<Button onClick={onSubmit} className="w-[452px]">
-					Daftar
-				</Button>
-			</div>
-			<div className="flex justify-center mt-[40px] gap-[8px]">
-				<h3>Sudah punya akun?</h3>
-					<h3 className="text-darkblue-05 font-bold">
-						Masuk di sini
-					</h3>
-			</div>
-		</div>
+						Nama tidak boleh kosong
+					</label>
+				)}
+
+          {/* Ketika Nama Benar */}
+          <div className="absolute bottom-0 sm:ms-[12rem] ms-[9rem] mb-[0.6rem] ...">
+            <CheckList />
+          </div>
+          </div>
+          </div>
+
+        {/* Input Nomor Telepon */}
+          <div className="w-[50%] ms-1 flex justify-between">
+          <div className="relative">
+            <Input
+              placeholder="081823456789"
+              type="number"
+              value={phone}
+              label="Nomor Telepon"
+              name="Nomor Telepon"
+              onChange={(e) => setPhone(e.target.value)}
+              onBlur={() => handlePhoneValidation(phone, setError)}
+              isError={error.isPhoneError}
+            />
+
+          {/* Ketika Nomor Telepon Error */}
+          {error && error.isPhoneError && (
+					<label
+						htmlFor="Nomor Telepon"
+						className="text-xs text-alert-warning"
+					>
+						Nomor Telepon tidak boleh kosong
+					</label>
+				)}
+
+          <div className="absolute bottom-0 sm:ms-[12rem] ms-[9rem] mb-[0.6rem] ...">
+            <CheckList />
+          </div>
+          </div>
+          </div>
+          
+        </div>
+        {/* Penutup Gabungan Nama dan Nomor Telepon */}
+
+      {/* Input Email */}
+        <div className="mt-1 ">
+        <div className="relative">
+            <Input
+              placeholder="Contoh: johndoe@gmail.com"
+              type="email"
+              value={email}
+              label="Email"
+              name="Email"
+              onChange={(e) => setEmail(e.target.value)}
+              onBlur={() => handleEmailValidation(email, setError)}
+              isError={error.isEmailError}
+            />
+
+          {/* Ketika Email Benar */}
+          <div className="absolute bottom-0 sm:ms-[26.3rem] ms-[20.5rem] mb-[0.7rem] ...">
+            <CheckList />
+          </div>
+          </div>
+
+          {/* Ketika Email Error */}
+          {error && error.isEmailError && (
+					<label
+						htmlFor="email"
+						className="text-xs text-alert-warning"
+					>
+						Email tidak valid
+					</label>
+				)}
+        </div>
+
+      {/* Input Password */}
+        <div className="mt-1">
+        <div className="relative">
+            <Input
+              placeholder="Buat Password"
+              type="password"
+              value={password}
+              label="Password"
+              name="Password"
+              onChange={(e) => setPassword(e.target.value)}
+              onBlur={() => handlePasswordValidation(password, setError)}
+              isError={error.isPasswordError}
+            />
+
+          {/* Ketika Password Error */}
+          {error && error.isPasswordError && (
+					<label
+						htmlFor="email"
+						className="text-xs text-alert-warning"
+					>
+						Password tidak boleh kurang dari 8 atau lebih dari 20
+						karakter
+					</label>
+				)}
+          </div>
+        </div>
+
+      {/* Pembuka Gabungan Asal Kota dan Asal Negara */}
+        <div className="mt-1 flex flex-row">
+
+        {/* Input Asal Kota */}
+          <div className="w-[50%] me-1 flex justify-between">
+          <div className="relative">
+            <Input
+              placeholder="Jakarta"
+              type="text"
+              value={city}
+              label="Asal Kota"
+              name="Asal Kota"
+              onChange={(e) => setCity(e.target.value)}
+              onBlur={() => handleCityValidation(city, setError)}
+              isError={error.isCityError}
+            />
+
+          {/* Ketika Asal Kota Error */}
+          {error && error.isCityError && (
+					<label
+						htmlFor="Asal Kota"
+						className="text-xs text-alert-warning"
+					>
+						Kota tidak boleh kosong
+					</label>
+				)}
+
+        {/* Ketika Asal Tidak Error */}
+          <div className="absolute bottom-0 sm:ms-[12rem] ms-[9rem] mb-[0.6rem] ...">
+            <CheckList />
+          </div>
+          
+          </div>
+          </div>
+
+        {/* Input Asal Negara */}
+          <div className="w-[50%] ms-1 flex justify-between">
+          <div className="relative">
+            <Input
+              placeholder="Indonesia"
+              type="text"
+              value={country}
+              label="Asal Negara"
+              name="Asal Negara"
+              onChange={(e) => setCountry(e.target.value)}
+              onBlur={() => handleCountryValidation(country, setError)}
+              isError={error.isCountryError}
+            />
+
+          {/* Ketika Asal Negara Error */}
+          {error && error.isCountryError && (
+					<label
+						htmlFor="Asal Negara"
+						className="text-xs text-alert-warning"
+					>
+						Negara tidak boleh kosong
+					</label>
+				)}
+
+          <div className="absolute bottom-0 sm:ms-[12rem] ms-[9rem] mb-[0.6rem] ...">
+            <CheckList />
+          </div>
+          </div>
+          </div>
+
+        </div>
+      {/* Penutup Gabungan Asal Kota dan Asal Negara */}
+
+      {/* Tombol Button */}
+        <div className="h-[3.2rem] mt-5">
+          <Button onClick={onSubmit} className="w-full">Daftar</Button>
+        </div>
+        <div className="mt-3 flex justify-center gap-[8px]">
+        <h3>Sudah punya akun?</h3>
+          <h3 className="text-darkblue-05 font-bold">Masuk di sini</h3>
+      </div>
+      {/* Penutup Tombol Button */}
+
+      </div>
+    </div>
 	);
 }
 
