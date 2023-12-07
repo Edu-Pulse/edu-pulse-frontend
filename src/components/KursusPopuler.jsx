3fa9009a54a44d7f54577d8214162009a91d9580
BayuAditiyaa <shinatsuka7@gmail.com>
2023-12-07 19:28:43 +0700

    Feat: API kursus populer

--- src/components/KursusPopuler.jsx
+++ src/components/KursusPopuler.jsx
@@ -4,22 +4,18 @@ import CourseCard from "./UI/CourseCard";
 import axios from "axios";
 
 const KursusPopuler = () => {
-  const [course, setCourse] = useState();
-
-  //   console.log(course[0].description);
+  const [course, setCourse] = useState([]);
+  const [selectedNumber, setSelectedNumber] = useState(null);
 
   useEffect(() => {
     const getAllCourse = async () => {
       try {
-        // const token = localStorage.getItem("token");
-
         const response = await axios.get(
           `https://pragos-academy-api-production.up.railway.app/course/all`
         );
 
         const data = response.data.data;
 
-        console.log(data);
         setCourse(data);
       } catch (error) {
         console.log(error.message);
@@ -27,6 +23,7 @@ const KursusPopuler = () => {
     };
     getAllCourse();
   }, []);
+
   return (
     <section className="container my-4">
       <div className="flex justify-between items-center py-5">
@@ -37,67 +34,119 @@ const KursusPopuler = () => {
       </div>
       <div className="flex justify-around w-full my-8 overflow-x-auto">
         <Button
-          className="text-sm !bg-darkblue-01 !text-black hover:bg-darkblue-05"
+          className={
+            selectedNumber === null
+              ? "whitespace-nowrap"
+              : "text-sm !bg-darkblue-01 !text-black hover:!bg-darkblue-05 whitespace-nowrap"
+          }
           size="xs"
+          onClick={() => setSelectedNumber(null)}
         >
           All
         </Button>
         <Button
-          className="text-sm !bg-darkblue-01 !text-black hover:bg-darkblue-05 whitespace-nowrap"
+          className={
+            selectedNumber === 2
+              ? "whitespace-nowrap"
+              : "text-sm !bg-darkblue-01 !text-black hover:!bg-darkblue-05 whitespace-nowrap"
+          }
           size="xs"
+          onClick={() => setSelectedNumber(2)}
         >
           Data Science
         </Button>
-        <Button className="whitespace-nowrap" size="xs">
+        <Button
+          className={
+            selectedNumber === 0
+              ? "whitespace-nowrap"
+              : "text-sm !bg-darkblue-01 !text-black hover:!bg-darkblue-05 whitespace-nowrap"
+          }
+          size="xs"
+          onClick={() => setSelectedNumber(0)}
+        >
           UI/UX Design
         </Button>
         <Button
-          className="text-sm !bg-darkblue-01 !text-black hover:bg-darkblue-05 whitespace-nowrap"
+          className="text-sm !bg-darkblue-01 !text-black hover:!bg-darkblue-05 whitespace-nowrap"
           size="xs"
         >
           Android Development
         </Button>
         <Button
-          className="text-sm !bg-darkblue-01 !text-black hover:bg-darkblue-05 whitespace-nowrap"
+          className="text-sm !bg-darkblue-01 !text-black hover:!bg-darkblue-05 whitespace-nowrap"
           size="xs"
         >
           Web Development
         </Button>
         <Button
-          className="text-sm !bg-darkblue-01 !text-black hover:bg-darkblue-05 whitespace-nowrap"
+          className="text-sm !bg-darkblue-01 !text-black hover:!bg-darkblue-05 whitespace-nowrap"
           size="xs"
         >
           IOS Development
         </Button>
         <Button
-          className="text-sm !bg-darkblue-01 !text-black hover:bg-darkblue-05 whitespace-nowrap"
+          className={
+            selectedNumber === 1
+              ? "whitespace-nowrap"
+              : "text-sm !bg-darkblue-01 !text-black hover:!bg-darkblue-05 whitespace-nowrap"
+          }
           size="xs"
+          // onClick={filterForPM}
+          onClick={() => setSelectedNumber(1)}
         >
           Business Intelligence
         </Button>
       </div>
       <div className="grid md:grid-cols-3 grid-cols-1 gap-6 w-full my-8">
-        <CourseCard
-          category={course[0].category}
-          name={course[0].name}
-          lecturer={course[0].lecturer}
-          level={course[0].level}
-          rating={course[0].rating}
-        />
-        <CourseCard
-          category={course[0].category}
-          name={course[0].name}
-          lecturer={course[0].lecturer}
-          level={course[0].level}
-          rating={course[0].rating}
-        />
-        <CourseCard
-          category={course[0].category}
-          name={course[0].name}
-          lecturer={course[0].lecturer}
-          level={course[0].level}
-          rating={course[0].rating}
-        />
+        {selectedNumber !== null ? (
+          <>
+            <CourseCard
+              category={course[selectedNumber]?.category}
+              name={course[selectedNumber]?.name}
+              lecturer={course[selectedNumber]?.lecturer}
+              level={course[selectedNumber]?.level}
+              rating={course[selectedNumber]?.rating}
+            />
+            <CourseCard
+              category={course[selectedNumber]?.category}
+              name={course[selectedNumber]?.name}
+              lecturer={course[selectedNumber]?.lecturer}
+              level={course[selectedNumber]?.level}
+              rating={course[selectedNumber]?.rating}
+            />
+            <CourseCard
+              category={course[selectedNumber]?.category}
+              name={course[selectedNumber]?.name}
+              lecturer={course[selectedNumber]?.lecturer}
+              level={course[selectedNumber]?.level}
+              rating={course[selectedNumber]?.rating}
+            />
+          </>
+        ) : (
+          <>
+            <CourseCard
+              category={course[0]?.category}
+              name={course[0]?.name}
+              lecturer={course[0]?.lecturer}
+              level={course[0]?.level}
+              rating={course[0]?.rating}
+            />
+            <CourseCard
+              category={course[1]?.category}
+              name={course[1]?.name}
+              lecturer={course[1]?.lecturer}
+              level={course[1]?.level}
+              rating={course[1]?.rating}
+            />
+            <CourseCard
+              category={course[2]?.category}
+              name={course[2]?.name}
+              lecturer={course[2]?.lecturer}
+              level={course[2]?.level}
+              rating={course[2]?.rating}
+            />
+          </>
+        )}
       </div>
     </section>
   );
