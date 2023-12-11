e81876cd0151e4a7fcd0058131ec382be6e133f4
DedeKurnn <dedekurniawan.dike@gmail.com>
2023-12-11 14:49:48 +0700

    Fix

---
+++ test/pages/AuthFlow/Login.test.js
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+import Login from "../../../src/pages/AuthFlow/Login";
+
+describe("Login Form", () => {
+	it("should show the login page", () => {
+		expect(Login).toBe();
+	});
+});
