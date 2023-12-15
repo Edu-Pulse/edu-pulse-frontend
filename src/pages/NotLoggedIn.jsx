1d2fde969c51c3d9a7fbe0c9a2595f1c312fd4ac
DedeKurnn <dedekurniawan.dike@gmail.com>
2023-12-15 10:19:20 +0700

    feat: add protected routes, loading state on button

--- src/pages/NotLoggedIn.jsx
+++ src/pages/NotLoggedIn.jsx
@@ -1,11 +1,11 @@
-import errorImage from "@/assets/svg/404.svg";
+import errorImage from "@/assets/svg/login.svg";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import HeaderMobile from "@/components/HeaderMobile";
 import Header from "@/components/Header";
 
-import { ArrowLeftIcon } from "@heroicons/react/24/outline";
+import { ShieldExclamationIcon } from "@heroicons/react/24/outline";
 
 const NotFound = () => {
 	const [isMobile, setIsMobile] = useState(null);
@@ -23,14 +23,14 @@ const NotFound = () => {
 			{isMobile ? <HeaderMobile /> : <Header />}
 			<div className="h-screen w-full grid place-content-center">
 				<div className="text-center">
-					<img src={errorImage} alt="404 Not Found" />
-					<h1>Oops! Halaman yang kamu cari tidak ditemukan</h1>
+					<img src={errorImage} alt="Unauthorized" />
+					<h1>Oops! Kamu belum log in ke dalam website</h1>
 					<p
-						className="flex items-center justify-center mt-4 text-darkblue-05 font-semibold hover:underline underline-offset-2 hover:cursor-pointer"
-						onClick={() => navigate(-1)}
+						className="flex items-center justify-center text-darkblue-05 font-semibold hover:underline underline-offset-2 hover:cursor-pointer"
+						onClick={() => navigate("/auth/login")}
 					>
-						<ArrowLeftIcon className="w-5 h-5 mr-2" />
-						<span>Kembali ke halaman sebelumnya</span>
+						<ShieldExclamationIcon className="w-5 h-5 mr-2" />
+						<span>Log in dulu yuk</span>
 					</p>
 				</div>
 			</div>
