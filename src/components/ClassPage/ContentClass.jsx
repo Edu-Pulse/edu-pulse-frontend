ef5330264554f78f47c2659781d7bfbc2499cc50
raihanbadruz <raihanzaman79@gmail.com>
2023-12-28 21:27:08 +0700

    Fix: add onboarding

--- src/components/ClassPage/ContentClass.jsx
+++ src/components/ClassPage/ContentClass.jsx
@@ -1,19 +1,5 @@
 import Modals from "@/components/CoursePage/Modals";
-import Button from "@/components/UI/Button";
-import {
-  StarIcon,
-  ArrowRightCircleIcon,
-  CurrencyDollarIcon,
-  PlayIcon,
-} from "@heroicons/react/24/solid";
-import {
-  RectangleStackIcon,
-  ShieldCheckIcon,
-  ClockIcon,
-} from "@heroicons/react/24/outline";
-import { useState } from "react";
-import toast from "react-hot-toast";
-import { useNavigate } from "react-router-dom";
+// import onboarding from "@/assets/Onboarding.png";
 
 const ContentClass = ({ details }) => {
   return (
@@ -41,73 +27,28 @@ const ContentClass = ({ details }) => {
           </ol>
         </div>
       </div>
-      {/* <Modals isOpen={open} onClose={handleCloseModal}>
+      {/* <Modals isOpen={open} onClose={() => setOpen(!open)}>
         <div className="text-center">
-          <div className="mx-auto w-48">
-            <div className="flex">
-              <h3 className="text-lg font-bold mb-4">
-                Selangkah Lagi menuju
-                <br />
-                <span className="text-darkblue-05">Kelas Premium</span>
-              </h3>
-            </div>
+          <div className="flex justify-center items-center">
+            <h3 className="text-2xl font-bold mb-4">Onboarding</h3>
           </div>
-          <div className="bg-white rounded-2xl shadow-xl">
-            <img
-              className="object-cover h-40 w-full"
-              src={`${details?.image}`}
-              alt=""
-            />
-            <div className="p-4">
-              <div className="flex justify-between items-center">
-                <h5 className="text-darkblue-05 text-sm font-semibold">
-                  {details?.category}
-                </h5>
-                <span className="flex">
-                  <StarIcon className="h-5 w-5 text-yellow-500" />
-                  <p className="text-sm">{details?.rating}</p>
-                </span>
-              </div>
-              <div className="flex justify-between items-center">
-                <h5 className="font-semibold mt-1">{details?.name}</h5>
-              </div>
-              <div className="flex justify-between items-center">
-                <p className="text-sm">by {details?.lecturer}</p>
-              </div>
-              <div className="flex justify-between items-center">
-                <p>Code: {details?.code}</p>
-              </div>
-              <div className="flex justify-between text-sm mb-2">
-                <span className="flex my-2 gap-1">
-                  <ShieldCheckIcon className="h-5 w-5 text-green-500" />
-                  <p>{details?.level} Level</p>
-                </span>
-                <span className="flex my-2 gap-1">
-                  <RectangleStackIcon className="h-5 w-5 text-green-500" />
-                  <p>10 Modul</p>
-                </span>
-                <span className="flex my-2 gap-1">
-                  <ClockIcon className="h-5 w-5 text-green-500" />
-                  <p>120 Menit</p>
-                </span>
-              </div>
-              <Button
-                className="!bg-darkblue-03 whitespace-nowrap"
-                icon={<CurrencyDollarIcon className="h-5 w-5" />}
-                iconPosition="left"
-                size="sm"
-              >
-                {details?.price}
-              </Button>
-            </div>
+          <img
+            src={onboarding}
+            className="object-cover hover:scale-105 transition-all duration-300 mx-auto"
+          />
+          <div>
+            <p className="font-semibold my-5">Selamat Belajar</p>
+            <p>
+              Persiapkan semua tools yang disarankan agar bisa belajar dengan
+              maksimal
+            </p>
           </div>
-          <div className="flex flex-row justify-center gap-4 mt-4">
+          <div className="flex flex-row justify-center gap-4 mt-5">
             <button
-              className="text-white bg-darkblue-05 h-10 px-8 py-2 flex gap-4 items-center hover:bg-purple-900 w-fit rounded-full hover:cursor-pointer transition-all duration-300"
-              onClick={handleBuyCourse}
+              onClick={handleCloseModal}
+              className="text-white bg-darkblue-05 h-10 px-16 py-2 flex gap-4 items-center hover:bg-purple-900 w-fit rounded-full hover:cursor-pointer transition-all duration-300"
             >
-              Beli Sekarang
-              <ArrowRightCircleIcon className="w-6 h-6" />
+              Ikuti Kelas
             </button>
           </div>
         </div>
