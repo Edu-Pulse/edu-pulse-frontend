f3ce337368732690ea15b06244c736e9b3b336a8
raihanbadruz <raihanzaman79@gmail.com>
2024-01-02 18:52:35 +0700

    Fix Footer & margin

--- src/components/UI/Footer.jsx
+++ src/components/UI/Footer.jsx
@@ -1,11 +1,11 @@
 const Footer = () => {
-	return (
-		<footer className="w-full bg-darkblue-05 py-4">
-			<div className="container text-center text-white font-medium">
-				<p>Copyright &copy; 2023 - Pragos Academy</p>
-			</div>
-		</footer>
-	);
+  return (
+    <footer className="w-full bg-darkblue-05 py-4">
+      <div className="container text-center text-white font-medium">
+        <p>Copyright &copy; 2023 - EduPulse</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
